bc82275c0e4955e1c12d0b26829de25bd1c8540b
Luisa Giraldo <luiisagiraldori@gmail.com>
2023-10-10 18:24:13 -0500

    Add images in assets

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,46 @@
+/* eslint-disable react/prop-types */
 import "./Card.scss";
 
-function Card() {
-  return <>Card</>;
+// eslint-disable-next-line react/prop-types
+function Card({
+  image,
+  name,
+  verification,
+  title,
+  text,
+  background,
+  backgroundImage,
+  colorText,
+  colorTextOpacity,
+}) {
+  return (
+    <>
+      <div
+        className="container-card"
+        style={{
+          backgroundColor: background,
+          backgroundImage: `url(${backgroundImage})`,
+          color: colorText,
+        }}
+      >
+        <div className="card-header">
+          <div>
+            <img src={image} />
+          </div>
+          <div className="card-header__name">
+            <span>{name}</span>
+            <span style={{ color: colorTextOpacity }}>{verification}</span>
+          </div>
+        </div>
+        <div className="card-title">
+          <span>{title}</span>
+        </div>
+        <div className="card-text" style={{ color: colorTextOpacity }}>
+          {text}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
